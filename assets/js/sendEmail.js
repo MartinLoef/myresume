8fd082a32aac5fc4f79799bef2ad8c8108ec40af
Martin Loef <loefmartin@gmail.com>
2018-10-08 11:16:47 +0000

    sending emails added via emailjs.com

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,14 +1,32 @@
 function sendMail(contactForm) {
     emailjs.send("gmail", "rosie", {
-            "from_name": contactForm.name.value,
-            "from_email": contactForm.emailaddress.value,
-            "project_request": contactForm.projectsummary.value
-        })
-        .then(
-            function(response) {
-                console.log("SUCCESS", response);
-            },
-            function(error) {
-                console.log("FAILED", response);
-            });
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
 }
+
+
+// function sendMail(contactForm) {
+//     emailjs.send("gmail", "rosie", {
+//             "from_name": contactForm.fullname.value,
+//             "from_email": contactForm.emailaddress.value,
+//             "project_request": contactForm.projectsummary.value
+//         })
+//         .then(
+//             function(response) {
+//                 console.log("SUCCESS", response);
+//             },
+//             function(error) {
+//                 console.log("FAILED", response);
+//             });
+// }
